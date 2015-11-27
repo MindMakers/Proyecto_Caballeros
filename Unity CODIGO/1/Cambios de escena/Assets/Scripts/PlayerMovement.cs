6d5faf1d011c2b9aceeef5f124b2eca327b65aeb
CCGLP <carlosgolfer@gmail.com>
2015-11-27 18:33:16 +0100

    Primer Commit
    Primer Commit provisional con el código de Sergio Aparicio.

---
+++ Unity CODIGO/1/Cambios de escena/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	private Rigidbody2D rbody;
+	private Animator anim;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		anim.SetFloat ("input_x", movement.x);
+		anim.SetFloat ("input_y", movement.y);
+
+		rbody.MovePosition (rbody.position + movement * speed * Time.deltaTime);
+
+	}
+}
